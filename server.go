c94b51897135b063f561fb6886b3a9367e4b47b4
PhivosJiar <paulyuancho@gmail.com>
2021-10-22 14:59:54 +0800

    新增rtm token

--- server.go
+++ server.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	rtctokenbuilder "github.com/AgoraIO/Tools/DynamicKey/AgoraDynamicKey/go/src/RtcTokenBuilder"
+	rtmtokenbuilder "github.com/AgoraIO/Tools/DynamicKey/AgoraDynamicKey/go/src/RtmTokenBuilder"
 )
 
 type rtc_int_token_struct struct {
@@ -25,6 +26,9 @@ var channel_name string
 
 var role_num uint32
 var role rtctokenbuilder.Role
+
+var rtm_token string
+var rtm_uid string
 
 func generateRtcToken(int_uid uint32, channelName string, role rtctokenbuilder.Role) {
 
@@ -115,6 +119,70 @@ func errorResponse(w http.ResponseWriter, message string, httpStatusCode int) {
 
 }
 
+//rtm
+type rtm_token_struct struct {
+	Uid_rtm string `json:"uid"`
+}
+
+func generateRtmToken(rtm_uid string) {
+
+	appID := "2997bf2437a74c5489878c5ec224b34d"
+
+	appCertificate := "0720a23244414748a082776246c86b5a"
+	expireTimeInSeconds := uint32(3600)
+	currentTimestamp := uint32(time.Now().UTC().Unix())
+	expireTimestamp := currentTimestamp + expireTimeInSeconds
+
+	result, err := rtmtokenbuilder.BuildToken(appID, appCertificate, rtm_uid, rtmtokenbuilder.RoleRtmUser, expireTimestamp)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("Rtm Token: %s\n", result)
+
+		rtm_token = result
+
+	}
+}
+
+func rtmTokenHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "*")
+
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	if r.Method != "POST" && r.Method != "OPTIONS" {
+		http.Error(w, "Unsupported method. Please check.", http.StatusNotFound)
+		return
+	}
+
+	var t_rtm_str rtm_token_struct
+	var unmarshalErr *json.UnmarshalTypeError
+	str_decoder := json.NewDecoder(r.Body)
+	rtm_err := str_decoder.Decode(&t_rtm_str)
+
+	if rtm_err == nil {
+		rtm_uid = t_rtm_str.Uid_rtm
+	}
+
+	if rtm_err != nil {
+		if errors.As(rtm_err, &unmarshalErr) {
+			errorResponse(w, "Bad request. Please check your params.", http.StatusBadRequest)
+		} else {
+			errorResponse(w, "Bad request.", http.StatusBadRequest)
+		}
+		return
+	}
+
+	generateRtmToken(rtm_uid)
+	errorResponse(w, rtm_token, http.StatusOK)
+	log.Println(w, r)
+}
+
 func main() {
 
 	port := "8082"
@@ -124,6 +192,7 @@ func main() {
 
 	// 使用 int 型 uid 生成 RTC Token
 	http.HandleFunc("/fetch_rtc_token", rtcTokenHandler)
+	http.HandleFunc("/fetch_rtm_token", rtmTokenHandler)
 	fmt.Printf("Starting server at port 8082\n")
 
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
